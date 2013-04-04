6cad8aa263b84ac21a06d65e977a34f8f342e519
bradtaylor <bradt@broadinstitute.org>
2013-04-04 13:11:39 -0400

    Added code to brad.js to draw the dots map!
    Loads somewhat slowly but it works!

--- website/scripts/brad.js
+++ website/scripts/brad.js
@@ -1,42 +1,44 @@
 /* Draw the map */
 function drawMap() {
+	// get the data
+	d3.csv("../tweets.csv", function(error, rows) {
+              if (error) return console.warn(error);
+                var data = [];
+                
+                
+                rows.forEach(function(d) {
+                    var o = {radius: 1,
+							//city: d.city,
+							//areaCode: +d.areaCode,
+							country: "USA", 
+							fillKey: "USA",
+							//state: d.region,
+							longitude: +d.long,
+							//postalCode: +d.lat,
+							latitude: +d.lat,
+							//TODO- fix year- Make an exact date object.
+							author: d.author,
+							//year: new Date(+d.year, 0, 1), // convert "Year" column to Date
+							text: d.text,
+							searchTerm: d.search_term,
+							id: +d.id};
+					
+                    data.push(o);
+                });
 	
-	// TODO- fix all of this
-	/*******
-	var tweets = d3.csv("tweets2.csv", function(d) {
-		return {
-			radius: 1,
-			city: d.city,
-			areaCode: +d.areaCode,
-			country: "USA", // convert "Length" column to number
-			fillKey: "USA",
-			state: d.region,
-			longitude: +d.longitude,
-			postalCode: +d.postalCode,
-			latitude: +d.latitude,
-			author: d.author,
-			//TODO- fix year- Make an exact date object.
-			year: new Date(+d.year, 0, 1), // convert "Year" column to Date
-			text: d.text,
-			searchTerm: d.search_term,
-			id: +id
-		};
-	}, function(error, rows) {
-		console.log(rows);
-	});
-	********/
 	//Draw the datamap
     var map = new Map({
         scope: 'usa',
 		el: $('#mapDiv'),
-        // bubbles: myData.mydata,
-        // bubble_config: {
-            // popupTemplate: _.template([
+        bubbles: data,
+        bubble_config: {
+            borderColor: '#667FAF'
+			// popupTemplate: _.template([
                 // '<div class="hoverinfo"><strong><%= data.author %></strong>',
                 // '<br/>Text: <%= data.text %>',
                 // '<br/>Search Term: <%= data.searchTerm %>',
                 // '</div>'].join(''))
-        // },
+        },
         geography_config: {
             popupOnHover: false,
             highlightOnHover: false
@@ -50,4 +52,6 @@ function drawMap() {
         }
     });
 	map.render();
+	
+	});
 }
