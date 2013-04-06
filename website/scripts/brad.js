8f30867c9fea4f4e791d07912455a373ee59a77e
Brad Taylor <brad.taylor3@gmail.com>
2013-04-05 22:55:21 -0400

    Added tooltip to dots map
    Also started work on term select, but messy/incomplete

--- website/scripts/brad.js
+++ website/scripts/brad.js
@@ -1,5 +1,11 @@
 /* Draw the map */
 function drawMap() {
+	var search_terms = ['fuck','shit', 'bitch', 'ass', 	'asshole','dick', 'cunt', 
+						'nigger', 'nigga', 'faggot',  'spic', 
+						'slut', 'whore','fucker', 'mother fucker',
+						'kill', 'beat', 'rape', 'fight', 'stab', 'shoot',
+						'twitter', 'tweet'];
+	
 	// get the data
 	d3.csv("data/tweets.csv", function(error, rows) {
               if (error) return console.warn(error);
@@ -32,12 +38,12 @@ function drawMap() {
 		el: $('#mapDiv'),
         bubbles: data,
         bubble_config: {
-            borderColor: '#667FAF'
-			// popupTemplate: _.template([
-                // '<div class="hoverinfo"><strong><%= data.author %></strong>',
-                // '<br/>Text: <%= data.text %>',
-                // '<br/>Search Term: <%= data.searchTerm %>',
-                // '</div>'].join(''))
+            borderColor: '#667FAF',
+			popupTemplate: _.template([
+					'<div class="hoverinfo"><strong><%= data.author %></strong>',
+					'<br/>Text: <%= data.text %>',
+					'<br/>Search Term: <%= data.searchTerm %>',
+					'</div>'].join(''))
         },
         geography_config: {
             popupOnHover: false,
@@ -52,6 +58,28 @@ function drawMap() {
         }
     });
 	map.render();
+	
+	// getting started on a series of checkboxes for highlighting
+	// d3 is hard.
+	// d3.select('body')
+			// .append('div')
+			//.style('float', 'right')
+			// .append('form')
+			// .selectAll('input')
+			// .data(search_terms)
+			// .enter()
+					// .append('span').text(function(d){return d})
+					// .append('input')
+					// .attr('type','radio')
+					// .attr('class','termBox')
+					// .on("click", click);
+		
+		// function click(term) {
+			//d3.selectAll('#termBox');
+			// alert(term);
+			// tweetData.forEach(function (d) {alert(d.author)})
+			// data 	
+		// }
 	
 	});
 }
