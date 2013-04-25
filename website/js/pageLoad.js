755c9e520d553e25ae1f5a7254596503a80dd224
Ted Natoli <ted.e.natoli@gmail.com>
2013-04-25 02:00:18 -0400

    added button selectors to the line graph and implemented the bar chart

--- website/js/pageLoad.js
+++ website/js/pageLoad.js
@@ -1,8 +1,20 @@
+bnames_json = [ { name: "avg_vulgar_tweets", display: "Average vulgar tweets"},
+				{ name: "avg_control_tweets", display: "Average innocuous tweets"},
+				{ name: "norm_tweets", display: "Ratio"} ];
+
 window.onload = function() {
 	drawMap();
-	//drawCharts();
-	d = drawOne("data/norm_tweets_per_day.csv", "avg_vulgar_tweets");
+	
+	// draw the line graph
+	drawOne("data/norm_tweets_per_day.csv", "avg_vulgar_tweets");
+	var e = d3.select("#time_tab").selectAll(".viz_sidebar");
+	console.log(e);
+	addButtons(e, "data/norm_tweets_per_day.csv", bnames_json);
+
+	// draw the bar chart
+	drawBarChart("2013-04-02", "red");
+	
+
 	z = drawScatter("data/social_data_combined.csv", "median_income_dollars");
-	//console.log(d);
 	//console.log(z);
 }
--- website/js/ted2.js
+++ website/js/ted2.js
@@ -1,6 +1,37 @@
+var parseDate = d3.time.format("%Y-%m-%d").parse;
+
+function sortTwo(arr1, arr2) {
+  arr1.sort();
+  // sort arr2 relative to arr1
+  // borrowed from http://answers.unity3d.com/questions/63520/sort-two-arrays.html
+    var foundone;
+    var tempobj;
+    var tempfloat;
+     
+    foundone = true;
+    while(foundone)
+    {
+      foundone = false;
+      for(i = 0; i < arr2.length - 1; i++)
+      {
+        if(arr1[i] > arr2[i + 1])
+        {
+          tempobj = arr1[i + 1];
+          tempfloat = arr2[i + 1];
+          arr1[i + 1] = arr1[i];
+          arr2[i + 1] = arr2[i];
+          arr1[i] = tempobj;
+          arr2[i] = tempfloat;
+          foundone = true;
+        }
+      }
+    } 
+    
+    return(arr2);
+}
+
 function drawOne(file_path, colname) {
-	var parseDate = d3.time.format("%Y-%m-%d").parse;
-	d3.csv(file_path, function(rows) {
+  d3.csv(file_path, function(rows) {
 		var datacols = d3.keys(rows[0]).filter(function(key) { return key == colname; });
 		var datacol = datacols[0];
 		var data = {name: datacol, values: []};
@@ -11,12 +42,19 @@ function drawOne(file_path, colname) {
 							  count: c});
 		});
 		var e = d3.select("#time_tab").selectAll(".viz_body");
-		console.log(e);
 		drawLine(e, data, "red");
 	});
 }
 
-function drawLine (element, data, color)  {
+function addButtons(element, file_path, bnames_json) {
+  element.data(bnames_json).enter()
+    .append("button")
+    .text(function(d) { return d.display; })
+    .attr("class", "btn btn-danger btn-large btn-block")
+    .on("click", function(d) { drawOne(file_path, d.name); });
+}
+
+function drawLine(element, data, color)  {
     d3.select("#linegraph_svg").remove();
 
 	var tooltip = element
@@ -103,4 +141,108 @@ function drawLine (element, data, color)  {
         .attr("x", 3)
         .attr("dy", ".35em")
         .text(function(d) { return d.name; });*/
+}
+
+var chart_shown = false;
+
+function drawBarChart(date, color) {    
+
+  // is there another chart already shown? if so, destroy it
+  if (chart_shown) {    
+    d3.select("#barchart_svg").remove();
+  }
+
+  var element = d3.select("#words_tab").select(".viz_body");
+  
+  var tooltip = element
+    .append("div")
+    .attr("class", "tooltip_div")
+    .style("position", "absolute")
+    .style("z-index", "10")
+    .style("visibility", "hidden"); 
+  
+  var margin = {top: 20, right: 20, bottom: 75, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+   
+  var x = d3.scale.ordinal()
+    .rangeBands([0, width]);
+
+  var y = d3.scale.linear()
+    .range([height, 0]); 
+       
+  var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+  var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+      
+      
+  var h1 = element.append("h1").attr("id", "barchart_header");
+  
+  var svg = element.append("svg")
+    .attr("id", "barchart_svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+  d3.json("data/raw_tweet_distrib_by_day.json", function(error, json) {
+    
+    if (error) return console.warn(error);
+    
+    data = null;
+    
+    for (var i = 0; i < json.length; i++) { 
+      if (String(parseDate(json[i].date)) === String(parseDate(date))) {
+      data = json[i];
+      d3.select("#barchart_header").text("Vulgar Tweets on " + date);         
+      }
+    }
+ 
+    x.domain(data.search_term);
+    y.domain([0, d3.max(data.count, function(d) { return +d; })]);
+
+    svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+        
+    svg.selectAll("text")
+      .attr("transform", function(d) {
+        return "rotate(45)translate(" + this.getBBox().width/2 + "," +
+            this.getBBox().height/2 + ")";
+      });
+
+    counts = sortTwo(data.search_term, data.count);
+        
+    svg.selectAll("rect")
+      .data(counts)
+      .enter().append("rect")
+      .attr("fill", color)
+      .attr("class", "barchart_bar")
+      .attr("x", function(d, i) { return x(i); })
+      .attr("y", function(d) { return y(+d); })
+      .attr("width", x.rangeBand())
+      .attr("height", function(d) { return height - y(+d); } )
+      .on("mouseover", function(d){return tooltip.style("visibility", "visible").text(+d  + " tweets"); })
+      .on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
+      .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
+      
+    svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Number of Tweets");
+        
+    });
+    
+    chart_shown = true;
+    
 }
